c2dec651a50c3c153e1ea5d3487715a023d6ac81
dengqiurong <852601818@qq.com>
2022-04-07 16:52:32 +0800

    then返回值和all、race

--- 手写promise/MyPromise.js
+++ 手写promise/MyPromise.js
@@ -34,30 +34,118 @@ function MyPromise(executor) {
 
 MyPromise.prototype.then = function (onFullfilled, onRejected) {
   var self = this
-  var nextPromise = new MyPromise(function (res, rej) {
+  // 如果是一个空then的话那么就原封不动的返回给下一个then
+  if (!onFullfilled) {
+    onFullfilled = function (val) {
+      return val;
+    }
+  }
+  if (!onRejected) {
+    onRejected = function (reason) {
+      throw new Error(reason)
+    }
+  }
 
+  function ResolutionRetrunPromise(nextPromise, nextReturnValue, res, rej) {
+    // 因为nextReturnValue有返回值所以不可能会报错，所有不用try catch
+    if (nextReturnValue instanceof MyPromise) {
+      nextReturnValue.then(function (value) {
+        res(value)
+      }, function (reason) {
+        rej(reason)
+      })
+    } else {
+      // 如果nextReturnValue只是一个普通值就直接进行res方法
+      res(nextReturnValue)
+    }
+  }
+
+  var nextPromise = new MyPromise(function (res, rej) {
     // 在then里面只要回调函数的返回值不报错就是执行res
     if (self.status === 'Fulfilled') {
-      var nextResolveValue = onFullfilled(self.resolveValue)
-      res(nextResolveValue)
+      // 用setTimeout模拟微任务
+      setTimeout(function () {
+        try {
+          var nextResolveValue = onFullfilled(self.resolveValue)
+          // 因为setTimeout是个异步方法所有可以取到nextPromise的值
+          ResolutionRetrunPromise(nextPromise, nextResolveValue, res, rej)
+        } catch (e) {
+          rej(e)
+        }
+
+      })
+
     }
     if (self.status === 'Rejected') {
-      var nextRejectValue = onRejected(self.rejectReason)
-      res(nextRejectValue)
+      setTimeout(function () {
+        try {
+          var nextRejectValue = onRejected(self.rejectReason)
+          ResolutionRetrunPromise(nextPromise, nextRejectValue, res, rej)
+        } catch (e) {
+          rej(e)
+        }
+
+      })
+
     }
 
     // 当异步执行的时候就可能是pedding状态
     // 等异步函数执行resolve或reject时执行数组里的方法
     if (self.status === 'pending') {
       self.ResolveCallBackList.push(function () {
-        var nextResolveValue = onFullfilled(self.resolveValue)
-        res(nextResolveValue)
+        setTimeout(function () {
+          try {
+            var nextResolveValue = onFullfilled(self.resolveValue)
+            ResolutionRetrunPromise(nextPromise, nextResolveValue, res, rej)
+          } catch (e) {
+            rej(e)
+          }
+
+        })
       })
       self.RejectCallBackList.push(function () {
-        var nextRejectValue = onRejected(self.rejectReason)
-        res(nextRejectValue)
+
+        setTimeout(function () {
+          try {
+            var nextRejectValue = onRejected(self.rejectReason)
+            ResolutionRetrunPromise(nextPromise, nextRejectValue, res, rej)
+          } catch (e) {
+            rej(e)
+          }
+
+        })
       })
+
     }
   })
   return nextPromise
+}
+
+MyPromise.race = function (promiseArr) {
+  return new MyPromise(function (resolve, reject) {
+    promiseArr.forEach(function (promise, index) {
+      promise.then(resolve, reject)
+    })
+  })
+}
+
+MyPromise.all = function (promiseArr) {
+  return new MyPromise(function (resolve, reject) {
+    var length = promiseArr.length
+    var rejList = []
+    var resList = []
+    promiseArr.forEach(function (promise, index) {
+      promise.then(function (val) {
+        resList.push(val)
+        if (index === (length - 1)) {
+          rejList.length > 0 ? reject(rejList[0]) : resolve(resList)
+        }
+        // res(val)
+      }, function (reason) {
+        rejList.push(reason)
+        rejList.length > 0 ? reject(rejList[0]) : resolve(resList)
+        // rej(reason)
+      })
+    })
+  })
 }
