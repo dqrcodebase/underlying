f093beeaa949a1853ea7897370490c7dddb8e1db
D Q R Mac <852601818@qq.com>
2022-04-08 12:21:55 +0800

    no message

--- 手写promise/MePromise.js
+++ 手写promise/MePromise.js
@@ -1,21 +1,21 @@
-function MePromise(executor) {
+function MePromise (executor) {
   var self = this;
   self.status = 'pending'
   self.resolveValue = null
   self.rejectReason = null
   self.ResolveCallBackList = []
   self.RejectCallBackList = []
-  function resolve(val) {
+  function resolve (val) {
     if (self.status === 'pending') {
       self.status = 'Fulfilled'
       self.resolveValue = val
       self.ResolveCallBackList.forEach(function (ele) {
-        ele()
+        ele(self.resolveValue)
       })
     }
   }
 
-  function reject(reason) {
+  function reject (reason) {
     if (self.status === 'pending') {
       self.status = 'Rejected'
       self.rejectReason = reason
@@ -33,26 +33,76 @@ function MePromise(executor) {
 }
 
 
+function ResolutionRetrunPromise (nextPromise, nextResolveValue, res, rej) {
+  if (nextResolveValue instanceof MePromise) {
+    console.log(nextResolveValue);
+    if (nextResolveValue.status === 'Fulfilled') {
+      res(nextResolveValue.resolveValue)
+    }
+    if (nextResolveValue.status === 'Rejected') {
+      rej(nextResolveValue.rejectReason)
+    }
+  } else {
+    res(nextResolveValue)
+  }
+}
+
 MePromise.prototype.then = function (onFulfilled, onRejected) {
   var self = this
-  var nextPromise = new MePromise(function () {
-  console.log(self.status)
+  var nextPromise = new MePromise(function (res, rej) {
+    console.log(self.status)
+    if (!onFulfilled) onFulfilled = function (res) { return res }
+    if (!onRejected) onRejected = function (reason) { return reason }
 
     if (self.status === 'Fulfilled') {
-      onFulfilled(self.resolveValue)
+      setTimeout(function () {
+        try {
+          const nextResolveValue = onFulfilled(self.resolveValue)
+          ResolutionRetrunPromise(nextPromise, nextResolveValue, res, rej)
+          // res(nextResolveValue)
+        } catch (e) {
+          rej(e)
+        }
+      })
     }
     if (self.status === 'Rejected') {
-      onRejected(self.rejectReason)
+      setTimeout(function () {
+        try {
+          const nextRejectReason = onRejected(self.rejectReason)
+          ResolutionRetrunPromise(nextPromise, nextRejectReason, res, rej)
+
+        } catch (e) {
+          rej(e)
+        }
+      })
+
+
     }
 
     if (self.status === 'pending') {
-      self.ResolveCallBackList.push(function (res) {
-        onFulfilled(self.resolveValue)
+      self.ResolveCallBackList.push(function () {
+        setTimeout(function () {
+          try {
+            const nextResolveValue = onFulfilled(self.resolveValue)
+            ResolutionRetrunPromise(nextPromise, nextResolveValue, res, rej)
+
+          } catch (e) {
+            rej(e)
+          }
+        })
+      })
+      self.RejectCallBackList.push(function () {
+        setTimeout(function () {
+          try {
+            const nextRejectReason = onRejected(self.rejectReason)
+            ResolutionRetrunPromise(nextPromise, nextRejectReason, res, rej)
+
+          } catch (e) {
+            rej(e)
+          }
+        })
       })
 
-      self.RejectCallBackList.push(function (reason) {
-        onRejected(self.rejectReason)
-      })
     }
   })
   return nextPromise
