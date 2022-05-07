e46dda94dd1921fe92d582798cf7aa74ed904929
dengqiurong <852601818@qq.com>
2022-05-07 11:23:08 +0800

    no message

--- 手写async/index.js
+++ 手写async/index.js
@@ -1,23 +1,46 @@
 
 
-const getData = () => new Promise(resolve => setTimeout(() => resolve("data333"),1000))
+const getData = () => new Promise(resolve => setTimeout(() => resolve("data333"), 1000))
 
 async function test() {
   const data = await getData()
-  console.log('data',data)
-  const data2 = await  getData()
-  console.log('data2',data2)
+  console.log('data', data)
+  const data2 = await getData()
+  console.log('data2', data2)
   return 'success'
 }
 
-// var test = asyncToGenerator(
-//   function* testG() {
-//     const data = yield getData()
-//     console.log('data',data);
-//     const data2 = yield getData()
-//     console.log('data2',data2)
-//     return 'success'
-//   }
-// )
+function asyncToGenerator(generatorFunc) {
+  return function () {
+    const gen = generatorFunc.apply(this, arguments)
+    return new Promise((resolve, reject) => {
+      function step(key, arg) {
+        let generatorResult
+        try {
+          generatorResult = gen[key](arg)
+        } catch (error) {
+          return reject(error)
+        }
+
+        const { value, done } = generatorResult
+        if (done) {
+          return resolve(value)
+        } else {
+          return Promise.resolve(value).then(val => step('next', val), err => step('throw', err))
+        }
+      }
+      step('next')
+    })
+  }
+}
+var test = asyncToGenerator(
+  function* testG() {
+    const data = yield getData()
+    console.log('data',data);
+    const data2 = yield getData()
+    console.log('data2',data2)
+    return 'success'
+  }
+)
 
 test().then(res => console.log(res))
