720acd84d84d58fb46a3dda67794f43ba8e50347
dengqiurong <852601818@qq.com>
2022-05-10 18:49:50 +0800

    no message

--- 手写async/index.js
+++ 手写async/index.js
@@ -2,13 +2,13 @@
 
 const getData = () => new Promise(resolve => setTimeout(() => resolve("data333"), 1000))
 
-async function test() {
-  const data = await getData()
-  console.log('data', data)
-  const data2 = await getData()
-  console.log('data2', data2)
-  return 'success'
-}
+// async function test() {
+//   const data = await getData()
+//   console.log('data', data)
+//   const data2 = await getData()
+//   console.log('data2', data2)
+//   return 'success'
+// }
 
 function asyncToGenerator(generatorFunc) {
   return function () {
@@ -26,19 +26,24 @@ function asyncToGenerator(generatorFunc) {
         if (done) {
           return resolve(value)
         } else {
-          return Promise.resolve(value).then(val => step('next', val), err => step('throw', err))
+          // 有时需要将现有对象转为 Promise 对象，Promise.resolve()方法就起到这个作用
+          return Promise.resolve(value).then(
+            val => step('next', val),
+            err => step('throw', err))
         }
       }
       step('next')
     })
   }
 }
+
+
 var test = asyncToGenerator(
   function* testG() {
     const data = yield getData()
-    console.log('data',data);
+    console.log('data', data);
     const data2 = yield getData()
-    console.log('data2',data2)
+    console.log('data2', data2)
     return 'success'
   }
 )
